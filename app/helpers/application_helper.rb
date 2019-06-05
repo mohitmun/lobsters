50bd7875103be14c998f4f0b763b925918b0e797
Mohit Munjani <mohitcrox@gmail.com>
2019-06-05 15:31:51 +0530

    pr3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,7 +39,7 @@ module ApplicationHelper
 
     @header_links = {
       root_path => { :title => @cur_url == "/" ? Rails.application.name : "Home" },
-      recent_path => { :title => "Recent PR 2" },
+      recent_path => { :title => "Recent PR 3" },
       comments_path => { :title => "Comments" },
     }
 
